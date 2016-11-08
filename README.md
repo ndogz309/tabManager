e9eaecbb1caa9bf811e5435a0a405ae6cd099dc3
N Dawg <nicole.atack+1@gmail.com>
2016-11-08 12:17:53 -0800

    get active tabs and display titles in a table

---
+++ README.md
@@ -0,0 +1,7 @@
+# chromeExtension
+
+making a start on making a chrome extension to save urls.  
+
+https://developer.chrome.com/extensions/getstarted
+https://robots.thoughtbot.com/how-to-make-a-chrome-extension
+
---
+++ popup.js
@@ -0,0 +1,43 @@
+
+
+
+document.getElementById('get-tabs').onclick = getTabs;
+
+function getTabs() {
+
+
+chrome.tabs.query({
+windowType: 'normal'
+
+}, function(tabs) {
+
+var tableRef = document.getElementById('mytable');
+var tabNum = tabs.length;
+console.log("number of tabs opennnnn");
+console.log(tabNum);
+
+ for (var i = 0; i < tabNum; i++) {
+  
+
+
+console.log("this is a tab")
+
+  // Insert a row in the table at row index 0
+  var newRow   = tableRef.insertRow(tableRef.rows.length);
+
+  // Insert a cell in the row at index 0
+  var newCell  = newRow.insertCell(0);
+
+  // Append a text node to the cell
+  var newText  = document.createTextNode(tabs[i].title);
+  newCell.appendChild(newText);
+   	
+
+  }
+
+
+});
+
+};
+
+
